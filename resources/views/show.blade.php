dc06ea4076840b4ef5e4c5fe573febcbe16400a6
abdul-raffay <abdul.raffay@nytrotech.com>
2020-10-23 12:31:54 +0500

    Update show.blade.php

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
         <div class="movie-info border-b border-gray-800">
-            <div class="container mx-auto px-4 py-16 flex">
+            <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
                 <img class="w-96" src="/img/parasite.jpg" alt="Parasite">
-                <div class="ml-16">
+                <div class="md:ml-16">
                     <h2 class="text-4xl font-semibold">
                         Parasite (2019)
                     </h2>
-                    <div class="flex items-center text-gray-400 text-sm mt-1">
+                    <div class="flex flex-wrap items-center text-gray-400 text-sm mt-1">
                         <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/></g></svg>
                         <span class="mx-1">85%</span>
                         <span class="ml-1 mr-2">|</span>
